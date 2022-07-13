fc807f44109f3b3eab05ee25f19ada6dae8a6eec
Murilo Medeiros do Valle <muthmedeiros@gmail.com>
2022-07-13 14:49:14 -0300

    Módulos 3 e 4

--- src/routes.js
+++ src/routes.js
@@ -1,23 +1,26 @@
+import Footer from 'components/Footer';
 import Menu from 'components/Menu';
+import PaginaPadrao from 'components/PaginaPadrao';
 import Cardapio from 'pages/Cardapio';
 import Inicio from 'pages/Inicio';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import styles from './Routes.module.scss';
+import NotFound from 'pages/NotFound';
+import Sobre from 'pages/Sobre';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 export default function AppRouter() {
   return (
-    <main>
+    <main className='container'>
       <Router>
         <Menu />
-        <header className={styles.header}>
-          <div className={styles.header__text}>
-            A casa do código e da massa
-          </div>
-        </header>
         <Routes>
-          <Route path='/' element={<Inicio />}/>
-          <Route path='/cardapio' element={<Cardapio />}/>
+          <Route path="/" element={<PaginaPadrao />}>
+            <Route index element={<Inicio />} />
+            <Route path="cardapio" element={<Cardapio />} />
+            <Route path="sobre" element={<Sobre />} />
+          </Route>
+          <Route path="*" element={<NotFound />} />
         </Routes>
+        <Footer />
       </Router>
     </main>
   );
